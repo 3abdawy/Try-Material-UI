83018c637aa69e849c93833b04b2b2c9e4582b50
aelabd <elabd1996@gmail.com>
2020-08-19 00:21:29 +0200

    Added toolbar margin

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,7 @@ import AppBar from "@material-ui/core/AppBar"
 import ToolBar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import useScrollTrigger from "@material-ui/core/useScrollTrigger"
+import { makeStyles } from "@material-ui/styles"
 
 function ElevationScroll(props) {
   const { children } = props
@@ -16,17 +17,24 @@ function ElevationScroll(props) {
     elevation: trigger ? 4 : 0,
   })
 }
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}))
 
 export default function Header(props) {
+  const classes = useStyles()
   return (
-    <ElevationScroll>
-      <AppBar>
-        <ToolBar>
-            <Typography variant="h3">
-            Arc Development
-            </Typography>
-            </ToolBar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar>
+          <ToolBar>
+            <Typography variant="h3">Arc Development</Typography>
+          </ToolBar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   )
 }
